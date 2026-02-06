e7ea0a9e102bd5b68dd3b5557da9148265da312b
remi <remitoutin@gmail.com>
2026-02-06 22:20:13 +0100

    Downloaded lost_in_conversation sharded dataset

--- data_download.py
+++ data_download.py
@@ -2,8 +2,12 @@
 
 from huggingface_hub import hf_hub_download
 import pandas as pd
+from datasets import load_dataset
 
 REPO_ID = "microsoft/lost_in_conversation"
 FILENAME = "lost_in_conversation.json"
 
-dataset = hf_hub_download(repo_id=REPO_ID, filename=FILENAME, repo_type="dataset")
+# dataset = hf_hub_download(repo_id=REPO_ID, filename=FILENAME, repo_type="dataset")
+# Source - https://stackoverflow.com/q/77755675
+ds = load_dataset(REPO_ID, data_files={'lost_in_conversation': FILENAME})
+ds.to_json("sharded_dataset.json")
