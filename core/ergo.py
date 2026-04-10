6228e97ff692f48245bdfc6ab352d7ae41da458b
remi <remitoutin@gmail.com>
2026-04-10 20:20:01 +0200

    Need to find good thresholds for all 3 signals

--- core/ergo.py
+++ core/ergo.py
@@ -10,6 +10,7 @@ class Ergo:
     def __init__(self, model: BaseModel, threshold):
         """
         Initialize ERGO with a model and entropy threshold.
+        TODO: thresholds for all 3 signals
         model: Any BaseModel (OpenAIModel, LocalModel)
         threshold: Threshold 𝚫Entropy must exceed to trigger rewriting.
         rewrite_prompts: Dataset specific few-shot prompts for rewriting. (retrieved from prompts.py)
@@ -17,6 +18,9 @@ class Ergo:
 
         self.model = model
         self.threshold = threshold
+        self.threshold_H = threshold # Shannon entropy threshold; trigger rewriting when 𝚫Entropy exceeds this (signal of high uncertainity)
+        self.threshold_p = threshold # token probability threshold; trigger rewriting when 𝚫Probability falls below this (signal of low confidence)
+        self.threshold_PPL = threshold # perplexity threshold; trigger rewriting when 𝚫Perplexity exceeds this (signal of better predictability)
 
         
         self.rewrite_prompts = {
