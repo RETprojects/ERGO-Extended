1262da787fb446d81833cbd4f3643846aa958b0b
remi <remitoutin@gmail.com>
2026-04-12 00:17:06 +0200

    Time for testing

--- main/example_main.py
+++ main/example_main.py
@@ -16,12 +16,12 @@ Example_Experiment = RunExperiment(
     clear_cache=True
 )
 
-Example_Experiment.run_Code(
+Example_Experiment.run_Actions(
     dataset_path="sharded_dataset.json", # Path to sharded dataset from Laban et al.
     num_Qs=100, 
     num_runs=3, 
     threshold_H=0.5, 
     threshold_p=-0.05, 
     threshold_PPL=15, 
-    output_path="outputs/code_example_3signals_thresholds_llama.json"
+    output_path="outputs/actions_example_3signals_thresholds_responses_llama.json"
 )
