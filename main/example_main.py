12b675bcebccc33775396dc73d52b47f3903b591
remi <remitoutin@gmail.com>
2026-04-13 20:21:33 +0200

    Will test ERGO's selected Llama entropy threshold w/ just inputs in consolidation

--- main/example_main.py
+++ main/example_main.py
@@ -20,7 +20,7 @@ Example_Experiment.run_Code(
     dataset_path="sharded_dataset.json", # Path to sharded dataset from Laban et al.
     num_Qs=100, 
     num_runs=3, 
-    threshold_H=0.5, 
+    threshold_H=0.03, 
     threshold_p=-0.05, 
     threshold_PPL=15, 
     output_path="outputs/code_example_3signals_thresholds_llama.json"
