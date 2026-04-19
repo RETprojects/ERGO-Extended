bf03ec46e695c492972a509d32be978caab89555
root <remitoutin@gmail.com>
2026-04-19 01:28:15 +0000

    Tested ERGO on code

--- main/example_main.py
+++ main/example_main.py
@@ -16,10 +16,10 @@ Example_Experiment = RunExperiment(
     clear_cache=True
 )
 
-Example_Experiment.run_Actions(
+Example_Experiment.run_Code(
     dataset_path="sharded_dataset.json", # Path to sharded dataset from Laban et al.
     num_Qs=100, 
     num_runs=3, 
     threshold=0.03, 
-    output_path="outputs/actions_example_ergo_selectthreshold_llama.json"
+    output_path="outputs/code_example_ergo_selectthreshold_llama.json"
 )
