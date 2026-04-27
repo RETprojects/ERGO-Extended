dd232feb4ab1fcf31921e0a95f1ba37cef2a1779
remi <remitoutin@gmail.com>
2026-04-28 00:11:00 +0200

    Brief script to count resets

---
+++ results_reader.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_json('outputs/math_example_3signals_newthresholds_llama_run2.json')
+
+# print(df.shape[0])
+# print(df.columns)
+
+resets = 0
+
+for index, row in df.iterrows():
+#   print(row['item_id'])
+#   print(f"{row["resets"].count(1)} out of {len(row["resets"])}")
+  resets += row["resets"].count(1)
+
+print("Total # of resets:", resets)
