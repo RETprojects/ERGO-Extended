c1799d8042328cb897b64edb425a1eb27b5e2560
remi <remitoutin@gmail.com>
2026-04-30 00:54:33 +0200

    Dealing w/ kwargs

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -615,8 +615,9 @@ class SummaryEvalUtils(EvalUtils):
         response["message"] = json.loads(response["message"])
         return response
     
-    def generate(self, messages, model="gpt-4o-mini", timeout=30, max_retries=3, temperature=1.0, is_json=False, return_metadata=False, max_tokens=None, variables={}):
-        kwargs = {}
+    def generate(self, messages, model="gpt-4o-mini", timeout=30, max_retries=3, temperature=1.0, is_json=False, return_metadata=False, max_tokens=None, variables={}, **kwargs):
+        if not (kwargs in locals() or kwargs in globals()):
+            kwargs = {}
         if is_json:
             kwargs["response_format"] = { "type": "json_object" }
         N = 0
