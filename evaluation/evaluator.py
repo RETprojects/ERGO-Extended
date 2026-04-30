5d20599c908a16983605fc3dc01ea3fe0bfea1ae
remi <remitoutin@gmail.com>
2026-04-30 04:33:54 +0200

    Corrected score indexing for SummaryEvaluator

--- evaluation/evaluator.py
+++ evaluation/evaluator.py
@@ -177,9 +177,8 @@ class SummaryEvaluator(Evaluator):
         reference_answer = dataset.data[question_id]
 
         score = SummEval.evaluator_function(extracted_answer, reference_answer)
-        print("score:", score)
         
-        return {"score": np.round(float(score), 4), "error": None}
+        return {"score": np.round(score['score'], 4), "error": None}
     
     def identifier(self):
         return "Summary"
