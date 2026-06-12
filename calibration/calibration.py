d4982bd6c70e21f55449a5f44b290a13cf20cffb
remi <remitoutin@gmail.com>
2026-06-12 15:21:38 +0200

    Begin calibration work w/ an explanatory comment

---
+++ calibration/calibration.py
@@ -0,0 +1,5 @@
+# TODO: find optimal signal thresholds
+# inputs: model, signals, GSM8K examples (NOT to be used for testing later, so as to avoid contamination)
+# tune multiple hyperparameters (signal thresholds)
+# these thresholds will be used for the given model
+# TODO: in runExperiment, call this file to tune thresholds before experiment begins
