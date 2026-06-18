34b7908d26cead865f8d6e83627ade3f4c8d4092
remi <remitoutin@gmail.com>
2026-06-18 11:11:48 +0200

    No need for multiple runs in filename

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -60,12 +60,11 @@ class Calibration():
     def calibrate(self):
         dataset_path="sharded_dataset.json"
         output_path="calibration.json"
-        run_no = 0 # track the runs for output file paths
         # for ent, prob, per in self.entropies, self.probabilities, self.perplexities:
         for ent in self.entropies:
             for prob in self.probabilities:
                 for per in self.perplexities:
-                    output_path_full=f"calibration_run{run_no}.json"
+                    output_path_full=f"calibration_run0.json"
                     # test this combination using the given model on the held-out set from GSM8K
                     dataset = GSM8K(dataset_path=dataset_path)
                     ergo = Ergo(model=self.model, threshold_H=ent, threshold_p=prob, threshold_PPL=per)
@@ -84,7 +83,6 @@ class Calibration():
                             score += int(m[-1]) # get the last character, which is a digit corresponding to the score for that turn
                     # store the accuracy in scores
                     self.scores[(ent, prob, per)] = score
-                    run_no += 1
         # choose the combination of thresholds w/ the highest accuracy
         combo = max(self.scores, key=self.scores.get)
         return combo
